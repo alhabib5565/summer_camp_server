490f6a32645d36019b3fa086c44278a0fb1f087a
al-habib <alhabib5565@gmail.com>
2023-10-07 15:56:29 +0600

    '10/7/2023'

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const classCollention = client.db('summerCampDB').collection('class')
 const selectClassCollection = client.db('summerCampDB').collection('selecClass')
 const enrolledClassCollection = client.db('summerCampDB').collection('enrolled')
 app.get('/', (req, res) => {
-  res.send('summer camp school')
+  res.send('E_class server')
 })
 
 const verifyInstructor = async (req, res, next) => {
@@ -68,10 +68,21 @@ const verifyInstructor = async (req, res, next) => {
   const user = await usersCollection.findOne(query)
 
   if (user?.role !== 'instructor') {
+    return res.status(403).send({ error: true, message: 'forbidden access' });
+  }
+  next();
+}
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  const query = { email: email }
+  const user = await usersCollection.findOne(query);
+  if (user?.role !== 'admin') {
     return res.status(403).send({ error: true, message: 'forbidden message' });
   }
   next();
 }
+
 
 // create payment intent
 app.post('/create-payment-intent', verifyJWT, async (req, res) => {
@@ -111,18 +122,36 @@ app.post('/enrolled', verifyJWT, async (req, res) => {
   res.send(result);
 })
 
-// --------------all enroll class -------------------
+// --------------all enroll class for user -------------------
 app.get('/enrollClass', verifyJWT, async (req, res) => {
   const email = req.query.email
   const query = { studentEmail: email }
   const result = await enrolledClassCollection.find(query).toArray()
   res.send(result)
 })
+
+// ---------------- total enroll class --------------
+app.get('/totalEnroled', verifyJWT, verifyAdmin, async (req, res) => {
+  const result = await enrolledClassCollection.find().toArray()
+  res.send(result)
+})
+
+
 //----------------------- popular class --------------------------
 app.get("/popularCls", async (req, res) => {
-  const jobs = await classCollention.find({}).sort({ enrolled: 1 }).toArray();
+  const jobs = await classCollention.find({}).sort({ enrolled: -1 }).toArray();
   res.send(jobs);
 });
+
+// ------------------------ popular instructor ------------------
+app.get('/populerInstructor', async (req, res) => {
+  const limit = 6; // Limit set to 6
+  const sortBy = { enrolled: -1 }; // Sort in descending order based on the students field
+
+  const result = await classCollention.find().sort(sortBy).limit(limit).toArray();
+  res.send(result);
+});
+
 //--------------------------increse enrolld sets after enrolled successfull-----------------
 app.patch('/reduceSets/:id', async (req, res) => {
   const id = req.params.id
@@ -144,17 +173,25 @@ app.patch('/reduceSets/:id', async (req, res) => {
 
 //---------------select class--------------------
 app.post('/select', async (req, res) => {
-  const clss = req.body;
-  const result = await selectClassCollection.insertOne(clss);
-  res.send(result);
+  const clss = req.body
+  const exist = await selectClassCollection.findOne({
+    $and: [
+      { classId: clss.classId },
+      { studentEmail: clss.studentEmail }
+    ]
+  })
+  console.log(exist)
+  if (exist) {
+    return res.send({ message: 'alredy bookmark' })
+  } else {
+    const result = await selectClassCollection.insertOne(clss);
+    res.send(result);
+  }
 })
 
 // delete from select
-
 app.get('/mySelectClass/:email', verifyJWT, async (req, res) => {
   const email = req.params.email;
-  // console.log('mySelected class')
-  // console.log('selectclass', email)
   if (!email) {
     res.send([]);
   }
@@ -163,7 +200,7 @@ app.get('/mySelectClass/:email', verifyJWT, async (req, res) => {
     return res.status(403).send({ error: true, message: 'forbidden access' })
   }
 
-  const query = { email: email };
+  const query = { studentEmail: email };
   const result = await selectClassCollection.find(query).toArray();
   res.send(result);
 });
@@ -208,7 +245,7 @@ app.post('/createUser', async (req, res) => {
   const query = { email: email }
   const existing = await usersCollection.findOne(query)
   if (existing) {
-    return res.send('user already exist')
+    return res.send({message:'user already exist'})
   }
   const result = await usersCollection.insertOne(user)
   res.send(result)
@@ -265,26 +302,89 @@ app.get('/allClass', async (req, res) => {
   res.send(result)
 })
 
-// --------------------get all approve class---------------------------
-app.get('/approveClass', async (req, res) => {
+app.get('/approveClassNumber', async (req, res) => {
   const status = { status: "approve" }
   const result = await classCollention.find(status).toArray()
-  res.send(result)
+  const totalApproveClass = { totalApproveClass: result.length }
+  res.send(totalApproveClass)
 })
 
+// just practice
+// app.get('/approveClassNumber', async (req, res) => {
+//   const status = { status: "approve" }
+//   const result = await classCollention.find(status).toArray()
+//   const totalApproveClass = { totalApproveClass: result.length }
+//   res.send(totalApproveClass)
+// })
+// app.get('/approveClsAggregate', async (req, res) => {
+//   const totalEnrolled = await classCollention.aggregate([
+//     {
+//       $match: { status: "approve" } // Filter only "approve" status
+//     },
+//     {
+//       $group: {
+//         "_id": {
+//           "status": "$status",
+//           "instructorName": "$instructorName",
+//           "instructorEmail": "$instructorEmail",
+//           "instructorPhoto": "$instructorPhoto",
+//           "enrolled": '$enrolled'
+//         },
+//         "enrolled": { $sum: 1 }
+//       }
+//     },
+//     {
+//       $sort: { enrolled: -1 } // Sort by "totalEnrolled" in descending order
+//     }
+//   ]).toArray()
+//   res.send(totalEnrolled)
+// })
 
+// --------------------get all approve class with paginations---------------------------
+app.get('/approveClass', async (req, res) => {
+  console.log(req.query)
+  const currentPage = parseInt(req.query.currentPage) || 0
+  const itemsPerPage = parseInt(req.query.itemsPerPage) || 3
+  const skip = currentPage * itemsPerPage
+  const status = { status: "approve" }
+  const result = await classCollention.find(status).skip(skip).limit(itemsPerPage).toArray()
+  res.send(result)
+})
+// ----------------- get class by id for showing class details--------------
+app.get('/classDetails/:id', async (req, res) => {
+  const id = req.params.id
+  console.log(id)
+  const query = { _id: new ObjectId(id) }
+  const result = await classCollention.findOne(query)
+  res.send(result)
+})
+//-------------------- get class by won instructor ---------------------------
 app.get('/class/:email', async (req, res) => {
   const email = req.params.email
   console.log(email)
-  const query = { email: email }
+  const query = { instructorEmail: email }
   const result = await classCollention.find(query).toArray()
   res.send(result)
 })
-
+// delete class from my class route in dashbord
 app.delete('/removeClas/:id', async (req, res) => {
   const id = req.params.id
   const query = { _id: new ObjectId(id) }
   const result = await classCollention.deleteOne(query)
+  res.send(result)
+})
+// -----------------add class feedback ------------------------
+app.patch('/class/feedback/:id', async (req, res) => {
+  const id = req.params.id
+  const body = req.body
+  console.log(id, body)
+  const filter = { _id: new ObjectId(id) }
+  const updateDoc = {
+    $set: {
+      feedback: body.feedback,
+    }
+  }
+  const result = await classCollention.updateOne(filter, updateDoc)
   res.send(result)
 })
 
@@ -302,23 +402,88 @@ app.patch('/class/approve/:id', async (req, res) => {
 })
 
 app.put('/updateClas/:id', async (req, res) => {
-  const newClass = req.body
+  const updateClas = req.body
   const id = req.params.id
   const filter = { _id: new ObjectId(id) }
   const options = { upsert: true };
+  console.log(updateClas)
   const updateDoc = {
     $set: {
-      className: newClass.className,
-      price: newClass.price,
-      sets: newClass.sets,
-      photo: newClass.photo
+      className: updateClas.className,
+      price: updateClas.price,
+      sets: updateClas.sets,
+      classPhoto: updateClas.classPhoto,
+      duration: updateClas.duration,
+      enrollEndDate: updateClas.enrollEndDate,
+      enrollStartDate: updateClas.enrollEndDate,
+      classType: updateClas.classType
     },
   };
   const result = await classCollention.updateOne(filter, updateDoc, options)
   res.send(result)
 })
 
+//-------------------- admin home--------------------
+// ------------------- totall ------------------
+/* app.get('/totalluser', async (req, res) => {
+  const totaluser = await usersCollection.estimatedDocumentCount()
+  res.send({ totaluser: totaluser })
+})
+
+// ------------------- totall Class ------------------
+app.get('/totallClass', async (req, res) => {
+  const totallClass = await classCollention.estimatedDocumentCount()
+  res.send({ totallClass: totallClass })
+})
+
+// ------------------- totall Class ------------------
+app.get('/totallEnrolled', async (req, res) => {
+  const totallEnroll = await enrolledClassCollection.estimatedDocumentCount()
+  res.send({ totallEnroll: totallEnroll })
+})
+
+// -------------------- totall sales -----------------
+app.get('/totalSales', async (req, res) => {
+  const totalSales = await enrolledClassCollection.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalPrice: { $sum: { $toDouble: '$price' } } // Convert price to a number if it's stored as a string
+      }
+    }
+  ]).toArray()
+  res.send({ totalSales: totalSales[0].totalPrice })
+}) */
+
+app.get('/total', async (req, res) => {
+  const totallClass = await classCollention.estimatedDocumentCount()
+  const totallEnroll = await enrolledClassCollection.estimatedDocumentCount()
+  const totaluser = await usersCollection.estimatedDocumentCount()
+  const totalPrice = await enrolledClassCollection.aggregate([
+    {
+      $group: {
+        _id: null,
+        totalPrice: { $sum: { $toDouble: '$price' } } // Convert price to a number if it's stored as a string
+      }
+    }
+  ]).toArray()
+  const totalSales = totalPrice[0].totalPrice
+  res.send({ totallClass, totallEnroll, totaluser, totalSales })
+})
+
+//-------------- last register-----------
+app.get('/lastJoinUser', verifyJWT, verifyAdmin, async (req, res) => {
+  const result = await usersCollection.find().sort({ whenYouRegister: 1 }).limit(5).toArray()
+  res.send(result)
+})
+
+// ----------last enrolled ------------
+app.get('/lastEnroled', verifyJWT, verifyAdmin, async (req, res) => {
+  const result = await enrolledClassCollection.find().sort({ date: -1 }).limit(5).toArray()
+  res.send(result)
+})
+
 
 app.listen(port, () => {
-  console.log('summer camp server running')
+  console.log('E_class server running')
 })
